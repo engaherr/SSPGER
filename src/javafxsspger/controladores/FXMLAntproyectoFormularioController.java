6da7646b0d5d5498961fc2bcbf37fbd0a1548527
engaherr <zs21013858@estudiantes.uv.mx>
2023-06-06 23:00:31 -0600

    Crear AnteproyectoFormulario

---
+++ src/javafxsspger/controladores/FXMLAntproyectoFormularioController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxsspger.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+/**
+ * FXML Controller class
+ *
+ * @author kikga
+ */
+public class FXMLAntproyectoFormularioController implements Initializable {
+
+    @FXML
+    private Label lbTitulo;
+    @FXML
+    private TextArea taProyectoInvestigacion;
+    @FXML
+    private ComboBox<?> cbAlumnosMax;
+    @FXML
+    private ComboBox<?> cbLgac;
+    @FXML
+    private TextArea taLineaInvestigacion;
+    @FXML
+    private TextArea taDuracionAprox;
+    @FXML
+    private ComboBox<?> cbModalidad;
+    @FXML
+    private TextArea taNombreTrabajo;
+    @FXML
+    private TextArea taRequisitos;
+    @FXML
+    private TextArea taDirector;
+    @FXML
+    private TextArea taCodirectores;
+    @FXML
+    private TextArea taDescripcionProyecto;
+    @FXML
+    private TextArea taDescripcionTrabajo;
+    @FXML
+    private TextArea taResultadosEsperados;
+    @FXML
+    private TextArea taBibliografiaRecomendada;
+    @FXML
+    private TextArea taComentarios;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
