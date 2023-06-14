48a306529b0265b310df3d68efb5faa507837882
engaherr <zs21013858@estudiantes.uv.mx>
2023-06-14 15:51:07 -0600

    merge final

--- src/javafxsspger/modelo/dao/EstudianteDAO.java
+++ src/javafxsspger/modelo/dao/EstudianteDAO.java
@@ -17,6 +17,39 @@ import javafxsspger.modelo.pojo.EstudianteRespuesta;
 import javafxsspger.utils.Constantes;
 
 public class EstudianteDAO {
+    public static EstudianteRespuesta obtenerEstudiantes(){
+        EstudianteRespuesta respuesta = new EstudianteRespuesta();
+        respuesta.setCodigoRespuesta(Constantes.OPERACION_EXITOSA);
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+        Connection conexionBD = ConexionBD.abrirConexionBD();
+        if(conexionBD != null){
+            try{
+                String consulta = "SELECT idEstudiante, email, nombre, apellidoPaterno, "
+                        + "apellidoMaterno, matricula, idAnteproyecto\n" +
+                "FROM estudiante;";
+                PreparedStatement prepararSentencia = conexionBD.prepareStatement(consulta);
+                ResultSet resultado = prepararSentencia.executeQuery();
+                while(resultado.next()){
+                    Estudiante estudiante = new Estudiante();
+                    estudiante.setIdEstudiante(resultado.getInt("idEstudiante"));
+                    estudiante.setEmail(resultado.getString("email"));
+                    estudiante.setNombre(resultado.getString("nombre"));
+                    estudiante.setApellidoPaterno(resultado.getString("apellidoPaterno"));
+                    estudiante.setApellidoMaterno(resultado.getString("apellidoMaterno"));
+                    estudiante.setMatricula(resultado.getString("matricula"));
+                    estudiante.setIdAnteproyecto(resultado.getInt("idAnteproyecto"));
+                    estudiantes.add(estudiante);
+                }
+                respuesta.setEstudiantes(estudiantes);
+                conexionBD.close();
+            } catch (SQLException ex) {
+                respuesta.setCodigoRespuesta(Constantes.ERROR_CONSULTA);
+            }
+        }else{
+            respuesta.setCodigoRespuesta(Constantes.ERROR_CONEXION);
+        }
+        return respuesta;
+    }
     
     public static EstudianteRespuesta obtenerEstudiantes(int idAcademico){
         EstudianteRespuesta respuesta = new EstudianteRespuesta();
